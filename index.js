35295bd404a3d854b55be2f23a73033658847c92
unknown <rikusrocks@gmail.com>
2020-09-24 22:33:29 +1000

    add 404

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-let note =[
+let notes =[
     {
         id:1,
         content: "1",
@@ -23,11 +23,22 @@ let note =[
 ]
 
 app.get('/',(req, res) => {
-    res.send('<h1>hello world</h1>')
+    res.send('<h1>Hello world</h1>')
 })
 
-app.get('/api/note', (req, res) => {
-    res.json(note)
+app.get('/api/notes/', (req, res) => {
+    res.json(notes)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const note = notes.find( item => item.id === id)
+    if(note){
+        res.json(note)
+    }else{
+        res.status(404).end()
+    }
+    
 })
 
 const PORT = 3001
