c4cbd948d2b53be31be174ee5477273ac6145fad
unknown <rikusrocks@gmail.com>
2020-09-25 17:59:06 +1000

    build frontend

---
+++ build/precache-manifest.883a5b3a255df07396fe45e1a5c4d7fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a161430f3dc985ca4133d90b07f21c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9087b7c103cb0d6c1c46",
+    "url": "/static/css/main.adb0e0ae.chunk.css"
+  },
+  {
+    "revision": "261379cd12f7a257a30b",
+    "url": "/static/js/2.72903e02.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.72903e02.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9087b7c103cb0d6c1c46",
+    "url": "/static/js/main.88c6dbe8.chunk.js"
+  },
+  {
+    "revision": "8dd7441d529e73d61d47",
+    "url": "/static/js/runtime-main.aaa24d6c.js"
+  }
+]);
